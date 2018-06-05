ea7986114cee7474130e22e773f0e61c3b02ba74
mae <gigadylan@gmail.com>
2018-06-06 00:06:35 +0900

    記事通りに作って見た

--- docs/sw.js
+++ docs/sw.js
@@ -1,40 +1,8 @@
-/**
- * service-worker.js
- */
-// self.addEventListener('install', (e) => {
-//   console.log('[ServiceWorker] Install');
-// });
-
-// self.addEventListener('activate', (e) => {
-//   console.log('[ServiceWorker] Activate');
-// });
-
-// // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-// self.addEventListener('fetch', (event) => {});
-
-self.addEventListener("push", (event) => {
-  event.waitUntil(
-    self.registration.pushManager.getSubscription()
-      .then((subscription) => {
-          if (subscription) {
-            return subscription.endpoint
-          }
-          throw new Error('User not subscribed')
-      })
-      .then((res) => {
-        return fetch('./notifications.json')
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json()
-        }
-        throw new Error('notification api response error')
-      })
-      .then((res) => {
-        return self.registration.showNotification(res.title, {
-          icon: './image/p-chan.png',
-          body: res.body
+self.addEventListener('push', function(event) {
+    event.waitUntil(
+        self.registration.showNotification(event.data.json().notification.title, {
+            'body': event.data.json().notification.body,
+            'icon': '/icon.png'
         })
-      })
-  )
-})
+    );
+});
