11775d2b91634a944255a87be9abd3cfc43771e9
mae <gigadylan@gmail.com>
2018-06-05 22:22:04 +0900

    a

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,25 @@
 /**
- * main.js
+ * 起動時.js
  */
-alert('main.js')
+// service workerが有効なら、service-worker.js を登録します
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("./sw.js")
+      .then(function(registration) {
+        // console.log('Service Worker Registered');
+        return registration.pushManager.getSubscription()
+          .then(function(subscription) {
+            if (subscription) {
+              return subscription
+            }
+            return registration.pushManager.subscribe({
+              userVisibleOnly: true
+            })
+          })
+      }).then(function(subscription) {
+        console.log("pushManager endpoint:", subscription.endpoint) 
+      }).catch(function(error) {
+        console.warn("serviceWorker error:", error)
+      })
+  }
+});
