d8fca6afa4bab705e902906dd9666ed23498c749
mae <gigadylan@gmail.com>
2018-06-07 08:36:48 +0900

    通知の画像は一旦なしで。sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function(event) {
     event.waitUntil(
         self.registration.showNotification(event.data.json().notification.title, {
-            'body': event.data.json().notification.body,
-            'icon': '/icon.png'
+            'body': event.data.json().notification.body
+            // 'icon': '/icon.png'
         })
     );
 });
