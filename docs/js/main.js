519db987be4668c69b47e889db70cc0baf6b8069
mae <gigadylan@gmail.com>
2018-06-08 00:13:45 +0900

    朝のやつでonMessage反応するのかテスト

--- docs/js/main.js
+++ docs/js/main.js
@@ -36,3 +36,18 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+
+// [START receive_message]
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.setBackgroundMessageHandler` handler.
+messaging.onMessage(function(payload) {
+  console.log('Message received. ', payload);
+  // [START_EXCLUDE]
+  // Update the UI to include the received message.
+  alert("反応した");
+  alert(payload);
+  // [END_EXCLUDE]
+});
+// [END receive_message]
