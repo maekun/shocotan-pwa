dcf949296ea0004c1b149514e87a1a6cfcef7c5d
mae <gigadylan@gmail.com>
2018-06-13 21:39:54 +0900

    service worker登録はやっぱりHTMLに記述

--- docs/main.js
+++ docs/main.js
@@ -1,59 +1,7 @@
 /**
  * 起動時.js
  */
-// Initialize Firebase
-const config = {
-  apiKey: "AIzaSyDde1epM6vV3Gv9YscN2N0zfXP5fUo3PEA",
-  authDomain: "shocotan-pwa.firebaseapp.com",
-  databaseURL: "https://shocotan-pwa.firebaseio.com",
-  projectId: "shocotan-pwa",
-  storageBucket: "shocotan-pwa.appspot.com",
-  messagingSenderId: "1098161935702"
-};
-firebase.initializeApp(config);
-// [START get_messaging_object]
-// Retrieve Firebase Messaging object.
-const messaging = firebase.messaging();
-// [END get_messaging_object]
-// [START set_public_vapid_key]
-// Add the public key generated from the console here.
-messaging.usePublicVapidKey(
-  'BFdunUMKBZ5rKcagYnz-WWyBxq_YLpbP7ut-0oV4ZOMthrkk7uNBWpmb7Dg8oVMHSC_G9BLPI5oHJliAkqlQP-8');
-// [END set_public_vapid_key]
 
-if ('serviceWorker' in navigator) {
-  // サービスワーカーの登録
-  navigator.serviceWorker.register('./sw.js').then((registration) => {
-    // サービスワーカー登録成功
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    // 今回はこのサービスワーカーを指定します \
-    messaging.useServiceWorker(registration);
-    // 通知の受信許可を確認する
-    messaging.requestPermission()
-      .then(() => {
-        console.log('Notification permission granted.');
-        // トークンを取得する
-        messaging.getToken()
-          .then((currentToken) => {
-            // 今回はコンソールに出力して取得します。実運用では適切な方法で保管する必要があります。
-            if (currentToken) {
-              console.log(currentToken);
-            } else {
-              console.log('No Instance ID token available. Request permission to generate one.');
-            }
-          })
-          .catch((err) => {
-            console.log('An error occurred while retrieving token. ', err);
-          });
-      })
-      .catch((err) => {
-          console.log('Unable to get permission to notify.', err);
-      });
-  }).catch((err) => {
-    // サービスワーカー登録失敗
-    console.log('ServiceWorker registration failed: ', err);
-  });
-}
 
 // [START receive_message]
 // Handle incoming messages. Called when:
